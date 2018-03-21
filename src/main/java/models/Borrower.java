745e6b96e685661736116e5225f300d2b0e9d205
Christiebelle <cslester@outlook.com>
2018-03-21 15:15:20 +0000

    borrower class added in

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,10 +1,12 @@
 package models;
 
+import java.util.Set;
+
 public class Borrower {
 
     private int id;
     private String name;
-    private Set<Book>books;
+    private Set<Book> books;
 
     public Borrower(String name){
         this.name = name;
